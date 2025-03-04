bc4f763d74d0b1ea71ce6776cf50012e564c9e79
Kenneth Cox <kccox@us.ibm.com>
2025-03-04 09:51:39 -0600

    iam_token_only defaults to true in solutions/enterprise

--- solutions/enterprise/variables.tf
+++ solutions/enterprise/variables.tf
@@ -252,3 +252,9 @@ variable "ibmcloud_kms_api_key" {
   sensitive   = true
   default     = null
 }
+
+variable "iam_token_only" {
+  type        = bool
+  description = "If set to true, disables Kafka's SASL PLAIN authentication method, only allowing clients to authenticate with SASL OAUTHBEARER via IAM access token. For more information, see: https://cloud.ibm.com/docs/EventStreams?topic=EventStreams-security. Only allowed for enterprise plans."
+  default     = true
+}
